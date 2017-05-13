90b48f9bd8bb607beed7b5e48930d0dc3ffc78f0
Rao-Li <charlottegirl103@gmail.com>
2017-05-13 19:50:47 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -7,15 +7,17 @@ import java.util.List;
 public class Student {
 	private String fName;
 	private String lName;
-	private int id;
+	private String id;
 	ArrayList<Class> schedule1=new ArrayList<Class>();
 	ArrayList<Class> schedule2=new ArrayList<Class>();
 	String[] quals;
+	
 	public Student(String[] s){
 		fName=s[2];
 		lName=s[4];
-		id=Integer.parseInt(s[3]);
+		id=s[3];
 	}
+	
 	public void addClass(String[] s){
 
 		if(s[1].equals("S1")){
@@ -25,6 +27,7 @@ public class Student {
 			schedule2.add(new Class(s[0], s[1], Integer.parseInt(s[5]), s[6], s[7]));
 		}
 	}
+	
 	public String toString(){
 		System.out.println( fName+" "+lName+", #"+id);
 		for(Class c:schedule1){
@@ -35,10 +38,30 @@ public class Student {
 		}
 		return "";
 	}
+	
 	public void sortClass(){
 		Collections.sort((List<Class>)schedule1);
 		Collections.sort((List<Class>)schedule2);
 		
 	}
 	
+	public String getLastName(){
+		return lName;
+	}
+	
+	public String getFirstName(){
+		return fName;
+	}
+	
+	public String getID(){
+		return id;
+	}
+	
+	public ArrayList<Class> getSchedule(int sem){
+		if(sem==1){
+			return schedule1;
+		}else if(sem==2){
+			return schedule2;
+		}
+	}
 }
