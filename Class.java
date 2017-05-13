f06c34cc4a8a5025ce6f179f5c3e4d0d00f7ce89
Rao-Li <charlottegirl103@gmail.com>
2017-05-13 19:52:53 -0400

    Update Class.java

--- Class.java
+++ Class.java
@@ -6,16 +6,31 @@ public class Class implements Comparable{
 	private Teacher teacher;
 	private int pd;
 	private String semester;
+	
 	public Class(String n, String s, int p, String f, String l){
 		name=n;
 		teacher=new Teacher(f,l);
 		semester=s;
 		pd=p;
 	}
+	
 	public String toString(){
 		return pd+ "\t"+semester+teacher;
 	}
+	
 	public int compareTo(Object arg0) {
 		return pd-((Class) arg0).pd;
+	}
+	
+	public int getPd(){
+		return pd;
+	}
+	
+	public String getClassName(){
+		return name;
+	}
+	
+	public String getSemester(){
+		return semester;
 	}
 }
