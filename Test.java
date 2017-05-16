4c84f14ca1329eacac6fe640c92494ab15c10cb4
g-tang <gracetang021@gmail.com>
2017-05-16 18:56:25 -0400

    Delete Test.java

--- Test.java
+++
@@ -1,7 +0,0 @@
-package application;
-
-public class Test {
-	public static void main(String[] args){
-		EmailUtil.sendEmail("gracetang021@gmail.com", "hi", "stuff stuff stuff and more stuff");
-	}
-}
