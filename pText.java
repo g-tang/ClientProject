f7ee698cc09e235ce00489909e23b6e7e25032c0
g-tang <gracetang021@gmail.com>
2017-05-30 21:05:20 -0400

    Add files via upload

--- pText.java
+++ pText.java
@@ -10,6 +10,7 @@ public class pText extends Text{
 		super(s);
 		setX((x/1920)*width);
 		setY((y/1080)*height);
+		setId("text");
 	}
 	public static void setDims(double a, double b){
 		width=a;height=b;
